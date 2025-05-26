9052f793787822df92834e8812ca93135a1e373b
Derek Wan <d.wan@icloud.com>
2025-05-26 20:55:29 +0900

    Commited by derekwan@DW-Mac at 2025-05-26 20:55:28 (Mon)

--- src/ibapi/__init__.py
+++ src/ibapi/__init__.py
@@ -1,11 +1,12 @@
+from __future__ import annotations
+
+
 """Copyright (C) 2023 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
 and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
 """
 
 """ Package implementing the Python API for the TWS/IB Gateway """
 
-
-from __future__ import annotations
 
 VERSION = {"major": 10, "minor": 30, "micro": 1}
 
@@ -15,4 +16,4 @@ def get_version_string():
 
 
 __version__ = get_version_string()
-__qrt_version__ = "1.0.0"
+__qrt_version__ = "1.0.1"
